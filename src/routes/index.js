b126e74601f2da4e360d3f1731e7b61adbba0730
hatake1608 <dominh162002@gmail.com>
2021-09-02 11:28:08 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const newRouter = require('./news');
 const siteRouter = require('./site');
 
 function route(app) {
-    app.use('/news', newRouter);
+    app.use(
+        '/news',
+
+        newRouter,
+    );
 
     app.use('/', siteRouter);
 }
